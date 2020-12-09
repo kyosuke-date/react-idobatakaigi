f08a0156eeb3b39426a055a042191555be102ce4
kyosuke-date <k.date1009@gmail.com>
2020-12-09 15:16:24 +0900

    remove hard coded env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,23 @@
-import firebase, { initializeApp } from 'firebase';
+import firebase from 'firebase';
+
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID
+} = process.env;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHSobJMZV0eHiTC086zqNPQK_nwjqtspg",
-  authDomain: "idobatakaigi-date.firebaseapp.com",
-  databaseURL: "https://idobatakaigi-date-default-rtdb.firebaseio.com",
-  projectId: "idobatakaigi-date",
-  storageBucket: "idobatakaigi-date.appspot.com",
-  messagingSenderId: "781302581647",
-  appId: "1:781302581647:web:86251720c4de7fdf1c63fa"
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID
 };
 //ファイアベースの初期化
 firebase.initializeApp(firebaseConfig);
