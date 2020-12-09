abe628ef22594bd941a8960781bc470a102441cc
kyosuke-date <k.date1009@gmail.com>
2020-12-09 21:33:21 +0900

    display messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { List } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
+import MessageItem from './MessageItem';
 import { messagesRef } from '../firebase';
 
 const useStyles = makeStyles({
   root: {
     gridRow: 1,
+    overflow: 'auto',
+    width: '100%'
   }
 });
 
@@ -14,10 +18,10 @@ const MessageList = () => {
   const classes = useStyles();
 
   useEffect(() => {
-    messagesRef.orderByKey().limitToLast(3).on('value', (snapshot) => {
+    messagesRef.orderByKey().limitToLast(15).on('value', (snapshot) => {
       const messages = snapshot.val();
       if(messages === null) return;
-      
+
       const entries = Object.entries(messages);
       const newMessages = entries.map((entry) => {
         const [key, nameAndText] = entry;
@@ -26,7 +30,15 @@ const MessageList = () => {
       setMessages(newMessages);
     });
   }, []);
-  return <div className={classes.root}>MessageList</div>
+  return (
+    <List className={classes.root}>
+      {
+        messages.map(({ key, name, text }) => {
+          return <MessageItem key={key} name={name} text={text}></MessageItem>;
+        })
+      }
+    </List>
+  );
 };
 
 export default MessageList;
